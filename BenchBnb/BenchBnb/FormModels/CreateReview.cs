0c2b50b34ce8b58847b7c00365203ca50e5f485b
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-10-04 14:44:35 -0400

    Final Cleanup

--- BenchBnb/BenchBnb/FormModels/CreateReview.cs
+++ BenchBnb/BenchBnb/FormModels/CreateReview.cs
@@ -7,7 +7,6 @@ namespace BenchBnb.FormModels
 {
     public class CreateReview
     {
-        public int Id { get; set; }
         public float Rating { get; set; }
         public string Comment { get; set; }
     }
